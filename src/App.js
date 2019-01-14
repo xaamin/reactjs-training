e7b51e044d6479c603260d0f465cb33379868d02
Benjamín Martínez Mateos <xaamin@outlook.com>
2019-01-14 17:11:53 -0600

    ReactJS - First interactions

--- src/App.js
+++ src/App.js
@@ -2,24 +2,128 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let COUNTERS = {
+
+}
+
+const count = (instance) => {
+  const name = instance.name || instance.constructor.name;
+
+  if (!COUNTERS[name]) {
+    COUNTERS[name] = 0;
+  }
+
+  COUNTERS[name] = COUNTERS[name] + 1;
+
+  console.log('COUNTERS', COUNTERS);
+}
+
 class App extends Component {
+
+  state = {
+    counter: 0,
+    products: [
+      {
+        id: 1,
+        name: 'USB',
+        quantity: 1,
+        price: 86.00
+      }
+    ]
+  }
+
+  componentWillMount() {
+    console.log('WILL MOUNT FIRED')
+  }
+
+  componentDidMount() {
+    console.log('DID MOUNT FIRED')
+
+    
+    setTimeout(() => {
+      this.setState({
+        products: [ ...this.state.products, {
+          id: 2,
+          name: 'MOUSE',
+          quantity: 1,
+          price: 70.00
+        }]
+      })
+    }, 500)
+  }
+
+  componentWillUpdate() {
+    console.log('WILL UPDATE FIRED')
+  }
+
+  componentDidUpdate() {
+    console.log('DID UPDATE FIRED')
+  }
+
+  componentWillUnmount() {
+    console.log('WILL UNMOUNT FIRED')
+  }
+
+  removeItem(product) {
+    console.log('REQUESTING DELETE FOR ', product)
+
+    const products = this.state.products.filter((item) => {
+      return product.id != item.id;
+    })
+
+    this.setState({
+      products
+    })
+  }
+
+  getTotal() {
+    let total = 0;
+
+    this.state.products.forEach((item) => {
+      total+= parseFloat(item.price) * parseFloat(item.quantity);
+    })
+
+    return total;
+  }
+
   render() {
+    count(this);
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h4>Cart</h4>
+
+        <table>
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Quantity</th>
+              <th>Price</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.state.products.map((product) => (
+              <tr key={ product.id }>
+                <td>{ product.name }</td>
+                <td>{ product.quantity }</td>
+                <td>{ product.price }</td>
+                <td>
+                  <button type="button" onClick={ () => this.removeItem(product) }>
+                    X
+                  </button>
+                </td>
+              </tr>
+            )) }
+          </tbody>
+          <tfoot>
+            <tr>
+              <th colSpan="3" className="footer">
+                Total: ${ this.getTotal() }
+              </th>
+            </tr>
+          </tfoot>
+        </table>
       </div>
     );
   }
