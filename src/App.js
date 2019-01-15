9e9b6d9d06dc66960d6de4b9c09f65126126c3ac
Benjamín Martínez Mateos <xaamin@outlook.com>
2019-01-15 10:24:33 -0600

    Added product list Added search Added favorite

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-let COUNTERS = {
+import ActionButton from './components/ActionButton'
 
-}
+let COUNTERS = {}
 
 const count = (instance) => {
   const name = instance.name || instance.constructor.name;
@@ -18,6 +18,57 @@ const count = (instance) => {
   console.log('COUNTERS', COUNTERS);
 }
 
+const available = [
+  {
+    id: 1,
+    name: 'Apple',
+    quantity: 1,
+    unit_price: 40.00,
+    price: 40.00,
+    favorite: false,
+  },
+  {
+    id: 2,
+    name: 'Bananas',
+    quantity: 1,
+    unit_price: 15.00,
+    price: 15.00,
+    favorite: false,
+  },
+  {
+    id: 3,
+    name: 'Carrots',
+    quantity: 1,
+    unit_price: 16.00,
+    price: 16.00,
+    favorite: false,
+  },
+  {
+    id: 4,
+    name: 'Drill',
+    quantity: 1,
+    unit_price: 10.00,
+    price: 10.00,
+    favorite: false,
+  },
+  {
+    id: 5,
+    name: 'Orange',
+    quantity: 1,
+    unit_price: 12.00,
+    price: 12.00,
+    favorite: false,
+  },
+  {
+    id: 6,
+    name: 'Watermelon',
+    quantity: 1,
+    unit_price: 25.00,
+    price: 25.00,
+    favorite: false,
+  }
+];
+
 class App extends Component {
 
   state = {
@@ -25,11 +76,20 @@ class App extends Component {
     products: [
       {
         id: 1,
-        name: 'USB',
-        quantity: 1,
-        price: 86.00
+      name: 'Apple',
+      quantity: 1,
+      unit_price: 40.00,
+      price: 40.00
       }
     ]
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.search = this.search.bind(this);
+    this.addItem = this.addItem.bind(this);
+    this.substractItem = this.substractItem.bind(this)
   }
 
   componentWillMount() {
@@ -44,10 +104,11 @@ class App extends Component {
       this.setState({
         products: [ ...this.state.products, {
           id: 2,
-          name: 'MOUSE',
+          name: 'Bananas',
           quantity: 1,
-          price: 70.00
-        }]
+          unit_price: 15.00,
+          price: 15.00
+        },]
       })
     }, 500)
   }
@@ -76,6 +137,51 @@ class App extends Component {
     })
   }
 
+  addItem(item) {
+    const products = this.state.products.map((product) => {
+      if (product.id === item.id) {
+        product.quantity++;
+        product.price = product.quantity * product.unit_price;
+
+      }
+
+      return product;
+    })
+
+    this.setState({
+      products
+    })
+  }
+
+  substractItem(item) {
+    const products = this.state.products.map((product) => {
+      if (product.id === item.id) {
+        product.quantity--;
+        product.price = product.quantity * product.unit_price;
+      }
+
+      return product;
+    })
+
+    this.setState({
+      products
+    })
+  }
+
+  addToCart(product) {
+    const found = this.state.products.find((item) => product.id === item.id)
+
+    if (!found) {
+      const { products } = this.state;
+
+      products.push(product);
+
+      this.setState({
+        products
+      })
+    }
+  }
+
   getTotal() {
     let total = 0;
 
@@ -86,12 +192,60 @@ class App extends Component {
     return total;
   }
 
+  search(event) {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+  getProducts() {
+    const { search } = this.state;
+
+    return available.filter((product) => {
+      if (search) {
+        return product.name.indexOf(search) !== -1
+      }
+
+      return true;
+    })
+  }
+
+  markAsFavorite(item) {
+    available.map((product) => {
+      if (product.id === item.id) {
+        product.favorite = !product.favorite;
+      }
+
+      return product;
+    })
+
+    const { products } = this.state;
+
+    this.setState({
+      products
+    })
+  }
+
+  handleClick(data) {
+    alert(data)
+  }
+
   render() {
+    const products = this.getProducts();
+
     count(this);
+
+    const promise = new Promise((resolve, reject) => {
+
+    })
 
     return (
       <div className="App">
         <h4>Cart</h4>
+
+        <ActionButton name="dummy" text="Dummy button" data="Hola" onClick={ this.handleClick } promise={ promise } />
 
         <table>
           <thead>
@@ -106,7 +260,11 @@ class App extends Component {
             { this.state.products.map((product) => (
               <tr key={ product.id }>
                 <td>{ product.name }</td>
-                <td>{ product.quantity }</td>
+                <td>
+                  <ActionButton type="button" name="substract" text="-" data={ product } onClick={ this.substractItem } />
+                  { product.quantity }
+                  <ActionButton type="button" name="add" text="+" data={ product } onClick={ this.addItem } />
+                </td>
                 <td>{ product.price }</td>
                 <td>
                   <button type="button" onClick={ () => this.removeItem(product) }>
@@ -116,6 +274,7 @@ class App extends Component {
               </tr>
             )) }
           </tbody>
+
           <tfoot>
             <tr>
               <th colSpan="3" className="footer">
@@ -124,6 +283,30 @@ class App extends Component {
             </tr>
           </tfoot>
         </table>
+
+        <hr />
+
+        <div>
+          <input type="text" name="search" autoComplete="off" className="input-search" placeholder="Search" onChange={ this.search } />
+          <div>
+            {
+              products.map((product) => (
+                <div key={ product.id } className="product-list-item">
+                  <img src="https://via.placeholder.com/150" />
+                  <h4>{ product.name }</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  </p>
+                  <div>
+                    <button type="button" className={ product.favorite ? 'favorite' : '' } onClick={ () => this.markAsFavorite(product) }>Favorito</button>
+                    <button type="button" onClick={ () => this.addToCart(product) }>Agregar</button>
+                  </div>
+                </div>
+              ))
+            }
+            
+          </div>
+        </div>
       </div>
     );
   }
